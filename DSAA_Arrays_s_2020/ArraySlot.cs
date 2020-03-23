585fa9a6adae67d58868a5c5a9f0e92af6c8e82e
Paul Powell <powell.paul@itsligo.ie>
2020-03-23 16:48:02 +0000

    Array of objects with countdown timer implemented

---
+++ DSAA_Arrays_s_2020/ArraySlot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAA_Arrays_s_2020
+{
+    public class ArraySlot
+    {
+        public int value { get; set; }
+        public Texture2D texture { get; set; }
+        public Rectangle Bounds { get; set; }
+
+        // New variable
+        public bool Visible { get; set; }
+        public Color BackgroundColor { get; set; }
+
+        
+        // we use the enabled property of a game 
+        // component to see if its activated
+        public ArraySlot()
+        {
+        }
+
+        public void Draw(SpriteBatch gameSB, SpriteFont font)
+        {
+            // New Draw code
+            if (!Visible) return;
+            // We get a spritebatch and font reference as a service
+            Vector2 textSize = font.MeasureString(value.ToString());
+            gameSB.Draw(texture, Bounds, BackgroundColor);
+            gameSB.DrawString(font, value.ToString(),
+                Bounds.Center.ToVector2() - textSize / 2, Color.White);
+        }
+
+    }
+}
--- DSAA_Arrays_s_2020/Game1.cs
+++ DSAA_Arrays_s_2020/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace DSAA_Arrays_s_2020
 {
@@ -11,6 +12,15 @@ namespace DSAA_Arrays_s_2020
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SpriteFont font;
+        ArraySlot SingleSlot;
+        public static int[] arrayValues = new int[] { 10, 20, 30, 40, 50, 60, 70 };
+        public ArraySlot[] arraySlots = new ArraySlot[arrayValues.Length];
+        
+        // New Game class variables
+        TimeSpan delay = new TimeSpan(0, 0, 3);
+        TimeSpan countDown;
+        int CurrentItem = 0;
 
         public Game1()
         {
@@ -39,8 +49,35 @@ namespace DSAA_Arrays_s_2020
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("GameFont");
+            //SingleSlot = new ArraySlot
+            //{
+            //    Bounds = new Rectangle(10, 10, 50, 50),
+            //    texture = Content.Load<Texture2D>("ArraySlotImage"),
+            //    value = 20,
+            //    BackgroundColor = Color.Aquamarine
+            //};
 
+            // Setup Color Array Slots
+            Point ScreenPosition = new Point(20, 20);
+            Point ArraySlotSize = new Point(100, 100);
+            Random r = new Random();
+            for (int i = 0; i < arrayValues.Length; i++)
+            {
+                arraySlots[i] =
+                new ArraySlot
+                {
+                    Bounds = new Rectangle(ScreenPosition, ArraySlotSize),
+                    texture = Content.Load<Texture2D>("ArraySlotImage"),
+                    value = arrayValues[i],
+                    BackgroundColor = new Color(r.Next(255), r.Next(255), r.Next(255)),
+                    Visible = false,
+                };
+                ScreenPosition += new Point(ArraySlotSize.X + 10, 0);
+            }
+            countDown = delay;
             // TODO: use this.Content to load your game content here
+
         }
 
         /// <summary>
@@ -62,6 +99,25 @@ namespace DSAA_Arrays_s_2020
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // Game Update Code	added
+            // Set current Slot item to true
+            arraySlots[CurrentItem].Visible = true;
+            // count down to 0
+            countDown -= gameTime.ElapsedGameTime;
+
+            if (countDown.Seconds <= 0)
+            {
+                // Make the current one invisible
+                arraySlots[CurrentItem].Visible = false;
+                if (CurrentItem < arraySlots.Length - 1)
+                {
+                    CurrentItem++;
+                }
+                else CurrentItem = 0;
+                // Set to 2 seconds
+                countDown = delay;
+            }
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -76,6 +132,15 @@ namespace DSAA_Arrays_s_2020
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            foreach (ArraySlot slot in arraySlots)
+                slot.Draw(spriteBatch, font);
+            spriteBatch.DrawString(
+                font,
+                    countDown.Seconds.ToString(),
+                        new Vector2(10, 10), Color.White);
+            //SingleSlot.Draw(spriteBatch, font);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
